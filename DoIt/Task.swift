f8ba9d1caf8084887a097044390af0bfb16ba7d6
Andrew Carras <ajcarras@me.com>
2017-07-18 20:55:37 -0400

    Stuff Done

---
+++ DoIt/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Stephen Carras on 7/18/17.
+//  Copyright © 2017 Stephen Carras. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name : String = ""
+    var important : Bool = false
+    
+    
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = createTask()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+        cell.textLabel?.text = task.name + "❗"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        return cell
+    }
+    
+    func createTask() -> [Task] {
+        
+        let task1 = Task()
+        task1.name = "Finish App"
+        task1.important = true
+        
+        let task2 = Task()
+        task2.name = "Do Stuff"
+        task2.important = false
+        
+        return [task1, task2]
+        
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
+    
 
 }
 
